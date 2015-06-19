1142d21bbd94f67d8825195f568fd2e0ee046002
Laurent Lienher <laurent.lienher@gmail.com>
2015-06-19 16:19:59 +0200

    notification controller

--- server/api/notification/index.js
+++ server/api/notification/index.js
@@ -9,8 +9,11 @@ var router = express.Router();
 
 router.get('/', auth.isAuthenticated(), controller.index);
 router.get('/:id', auth.isAuthenticated(), controller.show);
-router.post('/', auth.hasRole('manager'), controller.create);
+router.post('/', auth.isAuthenticated(), controller.create);
 router.put('/:id', auth.isAuthenticated, controller.update);
 router.put('/:id/viewed', auth.isAuthenticated(), controller.viewed);
+router.put('/:id/accepted', auth.isAuthenticated(), controller.viewed);
+router.put('/:id/refused', auth.isAuthenticated(), controller.viewed);
+router.delete('/:id', auth.isAuthenticated(), controller.destroy);
 
 module.exports = router;
--- server/api/notification/notification.controller.js
+++ server/api/notification/notification.controller.js
@@ -3,46 +3,133 @@
  * GET     /notification              ->  index
  * POST    /notification              ->  create
  * GET     /notification/:id          ->  show
+ * GET     /notification/me           ->  me  
  * PUT     /notification/:id          ->  update
  * PUT	   /notification/:id/viewed   ->  viewed
+ * PUT	   /notification/:id/accepted ->  accepted
+ * PUT	   /notification/:id/refused  ->  refused
+ * DELETE  /notification/:id          ->  destroy
  */
 
 'use strict';
 
 var Notification = require('./notification.model');
 
-/**
+ /**
  * Get a list of notification
  */
- exports.index = function(req, res){
-
+ exports.index = function(req, res, next){
+  Notification.find(function (err, notifications) {
+    if(err) { return handleError(res, err); }
+    return res.json(200, notifications);
+  });
  };
 
  /**
  * Create a new notification
- * rectification: 'manager'
  */
- exports.create = function(req, res){
+ exports.create = function(req, res, next){
+ 	var newNotification = new Notification({
+		title: req.body.title,
+		text: req.body.text,
+		//sentBy : req.body.sentBy,
+		sentTo : req.body.sentTo,
+		relatedToBusiness: req.body.businessID
+ 	});
+ 	newNotification.sentBy = req.user._id;
+ 	newNotification.status = 'not processed';
+ 	newNotification.isViewed = false;
 
+ 	newNotification.save(function (err, notificationSaved){
+ 		if (err) return next(err);
+ 		res.status(201).json(notificationSaved).end();
+ 	});
  };
  
  /**
  * Get a single notification
  */
- exports.show = function(req, res){
+ exports.show = function(req, res, next){
+ 	var notifID = req.params.id;
 
+	Notification.findById(notifID, function (err, notificationFound) {
+		if (err) return next(err);
+		if (!notificationFound) return res.send(401);
+		res.status(200).json(notificationFound);
+	});
  };
  
  /**
  * Update the notification attributes
  */
- exports.update = function(req, res){
+ exports.update = function(req, res, next){
 
  };
  
  /**
- * Change the notification status
+ * Change the viewed boolean of a notification
  */
- exports.viewed = function(req, res){
+ exports.viewed = function(req, res, next){
+ 	var notifID = req.params.id;
+
+ 	Notification.findById(notifID, function (err, notificationFound){
+ 		notificationFound.isViewed = true;
+ 		notificationFound.updatedOn = Date.now();
+ 		notificationFound.save(function (err, notificationSaved){
+	 		if (err) return next(err);
+	 		res.status(200).json(notificationSaved).end();
+ 		});
+ 	});
+ };
+
+ /**
+ * Change the status to accepted
+ */
+ exports.accepted = function(req, res, next){
+ 	var notifID = req.params.id;
+
+ 	Notification.findById(notifID, function (err, notificationFound){
+ 		notificationFound.status = 'accepted';
+ 		notificationFound.updatedOn = Date.now();
+ 		notificationFound.save(function (err, notificationSaved){
+	 		if (err) return next(err);
+	 		res.status(200).json(notificationSaved).end();
+ 		});
+ 	});
+ };
+
+ /**
+ * Change the status to refused
+ */
+ exports.refused = function(req, res, next){
+ 	var notifID = req.params.id;
+
+ 	Notification.findById(notifID, function (err, notificationFound){
+ 		notificationFound.status = 'refused';
+ 		notificationFound.updatedOn = Date.now();
+ 		notificationFound.save(function (err, notificationSaved){
+	 		if (err) return next(err);
+	 		res.status(200).json(notificationSaved).end();
+ 		});
+ 	});
+ };
+
+ /**
+ * Remove a notification
+ */
+ exports.destroy = function(req, res, next){
 
  };
+
+ function convertNotification(notif){
+	return { 	
+		id: notif.id,
+	 	name: notif.name,
+	 	text: notif.text,
+	 	sentBy: notif.sentBy,
+	 	sentTo: notif.sentTo,
+	 	relatedToBusiness: notif.relatedToBusiness,
+	 	status: notif.status,
+	 	isViewed: notif.isViewed
+	 }
+ }
