751da0f40a0e10b9e8e04623902364a34e8b74df
Laurent Lienher <laurent.lienher@gmail.com>
2015-06-30 11:47:12 +0200

    secondary navbar html and controller

---
+++ client/components/navbar/navbar-secondary.controller.js
@@ -0,0 +1,40 @@
+'use strict';
+
+angular.module('tbApp')
+  .controller('NavbarSecondryCtrl', function ($scope, $location, Auth) {
+
+    // bouton salon
+    $scope.status1 = {
+      isopen: false
+    };
+    
+    // bouton prestation
+    $scope.status2 = {
+      isopen: false
+    };
+
+    // bouton horaires
+    $scope.status3 = {
+      isopen: false
+    };
+
+    // bouton staff
+    $scope.status4 = {
+      isopen: false
+    };
+
+    // bouton rendezvous
+    $scope.status5 = {
+      isopen: false
+    };
+
+    $scope.toggleDropdown = function(status) {
+      //$event.preventDefault();
+      //$event.stopPropagation();
+      status.isopen = !status.isopen;
+    };
+
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
+  });
