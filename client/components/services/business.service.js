b4803b4d0556b979bb86c620406b9978605d744f
Laurent Lienher <laurent.lienher@gmail.com>
2015-07-02 15:21:11 +0200

    business service

--- client/components/services/business.service.js
+++ client/components/services/business.service.js
@@ -3,6 +3,7 @@
 angular.module('tbApp')
 	.factory('Business', ['$http', 'Urls', function ($http, Urls){
 		return {
+			// business functions
 			getBusinesses : function(callback, errorCallback){
 				$http({
 					method: 'GET',
@@ -26,7 +27,234 @@ angular.module('tbApp')
 			updateBusiness: function(businessId, callback, errorCallback){
 				$http({
 					method: 'PUT',
-					url: 'api/businesses/' + businessId + '/status'
+					url: Urls.api + 'businesses/' + businessId + '/status'
+				}).success(function (data){
+					callback(data);
+				}).error(function (data){
+					errorCallback(data);
+				});
+			},
+
+			// schedules functions
+			getSchedules: function(businessId, callback, errorCallback){
+				$http({
+					method: 'GET',
+					url: Urls.api + 'businesses/' + businessId + '/schedules'
+				}).success(function (data){
+					callback(data);
+				}).error(function (data){
+					errorCallback(data);
+				});
+			},
+			addSchedules: function(businessId, data, callback, errorCallback){
+				$http({
+					method: 'POST',
+					url: Urls.api + 'businesses/' + businessId + '/schedules',
+					data : data
+				}).success(function (data){
+					callback(data);
+				}).error(function (data){
+					errorCallback(data);
+				});
+			},
+			showSchedules: function(businessId, scheduleId, callback, errorCallback){
+				$http({
+					method: 'GET',
+					url: Urls.api + 'businesses/' + businessId + '/schedules/' + scheduleId
+				}).success(function (data){
+					callback(data);
+				}).error(function (data){
+					errorCallback(data);
+				});
+			},
+			updateSchedule: function(businessId, scheduleId, data, callback, errorCallback){
+				$http({
+					method: 'PUT',
+					url: Urls.api + 'businesses/' + businessId + '/schedules/' + scheduleId,
+					data: data
+				}).success(function (data){
+					callback(data);
+				}).error(function (data){
+					errorCallback(data);
+				});
+			},
+			deleteSchedule: function(businessId, scheduleId, callback, errorCallback){
+				$http({
+					method: 'DELETE',
+					url: Urls.api + 'businesses/' + businessId + '/schedules/' + scheduleId
+				}).success(function (data){
+					callback(data);
+				}).error(function (data){
+					errorCallback(data);
+				});
+			},
+
+			// Staff functions
+			getStaffs: function(businessId, callback, errorCallback){
+				$http({
+					method: 'GET',
+					url: Urls.api + 'businesses/' + businessId + '/staffs'
+				}).success(function (data){
+					callback(data);
+				}).error(function (data){
+					errorCallback(data);
+				});
+			},
+			showStaff: function(businessId, staffId, callback, errorCallback){
+				$http({
+					method: 'GET',
+					url: Urls.api + 'businesses/' + businessId + '/staffs/' + staffId
+				}).success(function (data){
+					callback(data);
+				}).error(function (data){
+					errorCallback(data);
+				});
+			},
+
+			// Prestations functions
+			getPrestations: function(businessId, callback, errorCallback){
+				$http({
+					method: 'GET',
+					url: Urls.api + 'businesses/' + businessId + '/prestations'
+				}).success(function (data){
+					callback(data);
+				}).error(function (data){
+					errorCallback(data);
+				});
+			},
+			createPrestation: function(businessId, data , callback, errorCallback){
+				$http({
+					method: 'POST',
+					url: Urls.api + 'businesses/' + businessId + '/prestations',
+					data: data
+				}).success(function (data){
+					callback(data);
+				}).error(function (data){
+					errorCallback(data);
+				});
+			},
+			showPrestation: function(businessId, prestationId, callback, errorCallback){
+				$http({
+					method: 'GET',
+					url: Urls.api + 'businesses/' + businessId + '/prestations/' + prestationId, 
+				}).success(function (data){
+					callback(data);
+				}).error(function (data){
+					errorCallback(data);
+				});
+			},
+			updatePrestation: function(businessId, prestationId, data, callback, errorCallback){
+				$http({
+					method: 'PUT',
+					url: Urls.api + 'businesses/' + businessId + '/prestations/' + prestationId, 
+					data: data
+				}).success(function (data){
+					callback(data);
+				}).error(function (data){
+					errorCallback(data);
+				});
+			},
+			// Price prestation status
+			createPrice: function(businessId, prestationId, data, callback, errorCallback){
+				$http({
+					method: 'GET',
+					url: Urls.api + 'businesses/' + businessId + '/prestations/' + prestationId + '/prices', 
+					data: data
+				}).success(function (data){
+					callback(data);
+				}).error(function (data){
+					errorCallback(data);
+				});
+			},
+			showPrice: function(businessId, prestationId, priceId, callback, errorCallback){
+				$http({
+					method: 'GET',
+					url: Urls.api + 'businesses/' + businessId + '/prestations/' + prestationId + '/prices/' + priceId  
+				}).success(function (data){
+					callback(data);
+				}).error(function (data){
+					errorCallback(data);
+				});
+			},
+			updatePrice: function(businessId, prestationId, priceId, callback, data, errorCallback){
+				$http({
+					method: 'PUT',
+					url: Urls.api + 'businesses/' + businessId + '/prestations/' + prestationId + '/prices/' + priceId, 
+					data: data 
+				}).success(function (data){
+					callback(data);
+				}).error(function (data){
+					errorCallback(data);
+				});
+			},
+			showPrice: function(businessId, prestationId, priceId, callback, errorCallback){
+				$http({
+					method: 'DELETE',
+					url: Urls.api + 'businesses/' + businessId + '/prestations/' + prestationId + '/prices/' + priceId 
+				}).success(function (data){
+					callback(data);
+				}).error(function (data){
+					errorCallback(data);
+				});
+			},
+			// Rendezvous functions
+			getRendezvous: function(businessId){
+				$http({
+					method: 'GET',
+					url: Urls.api + 'businesses/' + businessId + '/rendezvous'
+				}).success(function (data){
+					callback(data);
+				}).error(function (data){
+					errorCallback(data);
+				});
+			},
+			createRendezvous: function(businessId, data){
+				$http({
+					method: 'POST',
+					url: Urls.api + 'businesses/' + businessId + '/rendezvous',
+					data: data
+				}).success(function (data){
+					callback(data);
+				}).error(function (data){
+					errorCallback(data);
+				});
+			},
+			showRendezvous: function(businessId, rendezvousId){
+				$http({
+					method: 'GET',
+					url: Urls.api + 'businesses/' + businessId + '/rendezvous/' + rendezvousId
+				}).success(function (data){
+					callback(data);
+				}).error(function (data){
+					errorCallback(data);
+				});
+			},
+			// Notification functions
+			getNotification: function(businessId){
+				$http({
+					method: 'GET',
+					url: Urls.api + 'businesses/' + businessId + '/notifications'
+				}).success(function (data){
+					callback(data);
+				}).error(function (data){
+					errorCallback(data);
+				});
+			},
+			createNotification: function(businessId, data){
+				$http({
+					method: 'POST',
+					url: Urls.api + 'businesses/' + businessId + '/notifications',
+					data: data
+				}).success(function (data){
+					callback(data);
+				}).error(function (data){
+					errorCallback(data);
+				});
+			},
+			deleteNotification: function(businessId, notifId){
+				$http({
+					method: 'DELETE',
+					url: Urls.api + 'businesses/' + businessId + '/notifications/' + notifId
 				}).success(function (data){
 					callback(data);
 				}).error(function (data){
