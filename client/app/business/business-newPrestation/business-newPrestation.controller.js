66dd5115d56ce0bbc1c3d4e7886a9ed6c3035ba7
Laurent Lienher <laurent.lienher@gmail.com>
2015-07-02 17:32:50 +0200

    new prestation form & controller

--- client/app/business/business-newPrestation/business-newPrestation.controller.js
+++ client/app/business/business-newPrestation/business-newPrestation.controller.js
@@ -1,6 +1,40 @@
 'use strict';
 
 angular.module('tbApp')
-  .controller('BusinessNewPrestationCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('BusinessNewPrestationCtrl', function ($scope, Business, Auth) {
+   	$scope.prestation = {},
+   	$scope.errors = {}
+
+   	Auth.getCurrentUser(function(data){
+   		var user = data;
+
+	    $scope.submit = function(form) {
+	      $scope.submitted = true;
+	      console.log(form);
+	      if(form.$valid) {
+	        Business.createPrestation(
+	       	user.businessId,
+	        {
+	          name: $scope.prestation.name,
+	          shortDescription: $scope.prestation.shortDescription,
+	          description: $scope.prestation.description,
+	          duration: $scope.prestation.duration
+	        })
+	        .then( function() {
+	          $scope.prestation = {};
+	        })
+	        .catch( function(err) {
+	          err = err.data;
+	          $scope.errors = {};
+
+	          //Update validity of form fields that match the mongoose errors
+	          angular.forEach(err.errors, function(error, field) {
+	            form[field].$setValidity('mongoose', false);
+	            $scope.errors[field] = error.message;
+	          });
+	        });
+	      }
+	    };
+
+   	});
   });
--- client/app/business/business-prestations/business-prestations.controller.js
+++ client/app/business/business-prestations/business-prestations.controller.js
@@ -1,6 +1,21 @@
 'use strict';
 
 angular.module('tbApp')
-  .controller('BusinessPrestationsCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('BusinessPrestationsCtrl', function ($scope, Auth, Business) {
+
+    function getPrestations(data){
+      Business.getPrestations(
+        data.businessId,
+        function (data){
+          $scope.prestations = data.prestations;
+        },
+        function (error){
+          $scope.error = error;
+        });
+    }
+    
+    Auth.getCurrentUser(function (data){
+      return data;
+    }).then(getPrestations);
+    
   });
