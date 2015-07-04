05d7c683dcc1dc5849b29f819beb9b89cd9bd06d
Laurent Lienher <laurent.lienher@gmail.com>
2015-07-04 18:14:21 +0200

    various tweak

--- client/app/business/business-schedules/business-schedules.controller.js
+++ client/app/business/business-schedules/business-schedules.controller.js
@@ -2,11 +2,13 @@
 
 angular.module('tbApp')
   .controller('BusinessSchedulesCtrl', function ($scope, Auth, Business) {
-  	$scope.schedules = {};
+    $scope.schedules = {};
+  	$scope.form = {};
   	$scope.user = {};
-  	$scope.modif=false;
-	$scope.hover = '';
-	$scope.selection = '';
+    $scope.modif=false;
+  	$scope.add=false;
+  	$scope.hover = '';
+  	$scope.selection = '';
 
 
   	var staff = {'name': 'Test staff', '_id': '5591b95eaa39a54415a53bfb'};
@@ -38,73 +40,95 @@ angular.module('tbApp')
 
     }
 
-    
+    // Initial call
     Auth.getCurrentUser(function (data){
     	$scope.user = data;
       return data;
     }).then(getSchedulesAndStaff);
 
+  // Determine selection
+   $scope.index = function(index){
 
- $scope.index = function(index){
+   	var ids = [];
+   	for (var i = $scope.schedules[index].staffs.length - 1; i >= 0; i--) {
+   		ids.push($scope.schedules[index].staffs[i]._id);
+   	};
 
- 	var ids = [];
- 	for (var i = $scope.schedules[index].staffs.length - 1; i >= 0; i--) {
- 		ids.push($scope.schedules[index].staffs[i]._id)
- 	};
-
- 	$scope.selection = ids;
- 	//console.log($scope.selection);
- }
-
-
-  // toggle selection for a given staffs by id
-  $scope.toggleSelection = function toggleSelection(id) {
-
-    var idx = $scope.selection.indexOf(id);
-
-    // is currently selected
-    if (idx > -1) {
-      $scope.selection.splice(idx, 1);
-    }
-
-    // is newly selected
-    else {
-      $scope.selection.push(id);
-    }
-
-    //console.log($scope.selection);
-
-  };
-
-  $scope.options = createHourString();
-
-  // Créer un tableau avec les heures possibles du formulaire de modification
-  function createHourString (){
-	  var minutes = 0;
-	  var heures = 0;
-	  var string = [];
-	  do{
-	  	do{
-
-	  		string.push(String(heures+':'+minutes));
-
-	  		minutes += 5;
-	  	}while(minutes < 60);
-	  	minutes = 0;
-	  	heures += 1;
-	  }while(heures < 24);
-	  //console.log(string);
-	  return string;
-  };
+   	$scope.selection = ids;
+   	//console.log($scope.selection);
+   }
 
 
+    // toggle selection for a given staffs by id
+    $scope.toggleSelection = function toggleSelection(id, parentIndex, index) {
 
+      var idx = $scope.selection.indexOf(id);
 
+      // is currently selected
+      if (idx > -1) {
+        $scope.selection.splice(idx, 1);
+      }
 
+      // is newly selected
+      else {
+        $scope.selection.push(id);
+      }
 
+      //console.log($scope.selection);
 
+    };
 
+    $scope.options = createHourString();
 
+    // Créer un tableau avec les heures possibles du formulaire de modification
+    function createHourString (){
+      var mn = 0;
+      var hr = 0;
+      var strMn = [
+        '00', '15', '30', '45'
+      ];
+      var strHr = [
+        '00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11',
+        '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'
+      ];
+      var str=[];
 
+      for (var i = strHr.length; i > 0; i--) {
+        for (var j = strMn.length; j > 0; j--) {
+          str.push(String(strHr[strHr.length - i] + ":" + strMn[strMn.length - j]));
+        };
+      };
 
-  });
+      return str;
+    };
+
+    // Add a schedule
+    $scope.addSchedule = function(data){
+      console.log(data);
+/*      Business.addSchedule(
+        $scope.user.businessId,
+        data,
+        function(data){
+          $scope.schedules = data.horaire;
+        },
+        function(error){
+          $scope.error = error;
+        }
+      );*/
+    };
+
+    // Delete a schedule
+    $scope.deleteSchedule = function(scheduleId){
+      Business.deleteSchedule(
+        $scope.user.businessId,
+        scheduleId,
+        function(data){
+          $scope.schedules = data.horaire;
+        },
+        function(error){
+          $scope.error = error;
+        }
+      );
+    };
+
+});
