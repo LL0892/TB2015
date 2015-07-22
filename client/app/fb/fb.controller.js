3cc590a8ffdfbdb7f66b1049a4da0cb51ff107d0
Laurent Lienher <laurent.lienher@gmail.com>
2015-07-22 22:21:44 +0200

    fb app redirect

--- client/app/fb/fb.controller.js
+++ client/app/fb/fb.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('tbApp')
-.controller('FbCtrl', function($scope, Urls, $http) {
+.controller('FbCtrl', function($scope, $log, $http, $cookies, Urls) {
 	$scope.fb = function(){
 		$http.post(Urls.api + 'fb/rendezvous',{
 			signed_request: '9HOX6j2CfLglBxXI2wr11zTmix3VJWx8xI7C5sM_qyo.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImV4cGlyZXMiOjE0Mzc0ODM2MDAsImlzc3VlZF9hdCI6MTQzNzQ3NzA5NSwib2F1dGhfdG9rZW4iOiJDQUFVWkE2U1hhcmVNQkFFd3FWRFpBQU1zc3hOUzlPWkFEeFkybDZPM0dTeW1uMndyUDlGa0h0WVF2dkVlMmZOajh0aXJRNDFKU1NaQnZGanJESVhLWHFzUmVHSGg1RXFaQWM2eTVuZ2t6ek5ZcDJFVXlTNW1TRTFYWkJiZFpCU0NOWkJiNmZxeWo2MzY5UkNHWkFSY1pDY3d3RnRYSGs0WDV6UFlkcUNDb3l5M1NQbVpDUDhvUTl1SlBLRkxlNEhpaWg2dzg5d3NRU0dmV0FHemNzeE9MZFNYb1d4IiwicGFnZSI6eyJpZCI6Ijg4NDk2MDQxODIyNTExNyIsImFkbWluIjp0cnVlfSwidXNlciI6eyJjb3VudHJ5IjoiY2giLCJsb2NhbGUiOiJmcl9GUiIsImFnZSI6eyJtaW4iOjIxfX0sInVzZXJfaWQiOiIxMDIwNzMyMDY1MTA0MDQ0MCJ9'
@@ -11,4 +11,9 @@ angular.module('tbApp')
 
 		});
 	};
+
+	$scope.pageId = $cookies.get('page-id')
+	$cookies.remove('page-id');
+
+	$log.debug('fb call from page id '+ $scope.pageId);
 });
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,10 +1,25 @@
 'use strict';
 
 angular.module('tbApp')
-  .controller('MainCtrl', function ($scope, $http, $log, Urls, Auth, Business) {
+  .controller('MainCtrl', function ($scope, $http, $log, $state, $cookies, Urls, Auth, Business) {
+
+    /*
+    * Appel à l'initialisation du controller
+    * Si le cookie 'page-id' existe, cela signifie que l'app facebook a été appelée coté serveur
+    * On redirige l'utilisateur sur la page de l'app
+    */
+    function init (){
+      if ($cookies.get('page-id')) {
+        $state.go('fb');
+      }
+    }
+
+    init();
+
+    // initialiser $scopes
     $scope.businesses = [];
-    //$scope.currentUser = Auth.getCurrentUser;
     $scope.isLoggedIn = Auth.isLoggedIn;
+    //$scope.currentUser = Auth.getCurrentUser;
 
     $http.get('/api/businesses').success(function(businesses){
       $scope.businesses = businesses;
@@ -27,7 +42,6 @@ angular.module('tbApp')
           $scope.error = error;
         });
     }
-
 
     // Selection par defaut sur l'animation de click sur un salon
     $scope.selectedIndex = -1;
