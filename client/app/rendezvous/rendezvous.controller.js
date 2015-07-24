cefc0443e88f7167f75d4985a5a207243d2caac4
Laurent Lienher <laurent.lienher@gmail.com>
2015-07-24 11:05:21 +0200

    calendar service

--- client/app/rendezvous/rendezvous.controller.js
+++ client/app/rendezvous/rendezvous.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('tbApp')
-.controller('RendezvousCtrl', function($scope, $timeout, $http, $state, $log, $compile, Auth, User, Business, Staff, localStorageService, uiCalendarConfig) {
+.controller('RendezvousCtrl', function($scope, $timeout, $http, $state, $log, $compile, Auth, User, Business, Staff, localStorageService, uiCalendarConfig, CalendarService) {
 
   $scope.isStaff = Auth.isStaff;
   $scope.is = false;
@@ -34,11 +34,6 @@ angular.module('tbApp')
   }
 
 
-
-/* if ($state.is('rendezvous.step1')) {
-    // clear local storage
-  };*/
-
   // Si l'utilisateur reload la page
   if (!getItem('rendezvous')) {
   	$state.go('rendezvous.step1');
@@ -63,11 +58,6 @@ angular.module('tbApp')
   		}
   	}
   }
-
-
-/*if ($scope.formData = {}) {
-  	$state.go('rendezvous.step1');
-  };*/
 
 
   // Stocker les infos du salon
@@ -202,57 +192,22 @@ angular.module('tbApp')
         $scope.formData.businessId,
         request,
       function (res){
-        var array = res.rendezvous;
         //$log.debug(res);
+        var array = res.rendezvous;
 
-        createRendezvousTakenEvents(array);
-        //$log.debug($scope.events);
-        
-        createBusinessHoursEvents($scope.currentWeek.firstDay);
-        createMyRendezvousEvent(new Date());
+        $scope.events = CalendarService.createRendezvousTakenEvents($scope.events, array);            
+        $scope.businessHours = CalendarService.createBusinessHoursEvents($scope.businessHours, $scope.schedules, $scope.currentWeek.firstDay);
+        $scope.myRendezvous = CalendarService.createMyRendezvousEvent($scope.myRendezvous, new Date());
 
-        //$log.debug($scope.myRendezvous);
+        $log.debug($scope.events);
+        $log.debug($scope.businessHours);
+        $log.debug($scope.myRendezvous);
       },
 
       function(error){
         $scope.error = error;
       });
 	};
-
-  // Créer les évènements sur les rendez-vous déjà prit pour ce staff
-  function createRendezvousTakenEvents (array){
-        $scope.events.splice(0, $scope.events.length);
-        var data = array;
-
-        var rendezvous = [];
-        var startHour = '';
-        var endHour = '';
-        for (var i = data.length - 1; i >= 0; i--) {
-          // Converstion pour des dates compatibles avec le calendrier
-          startHour = data[i].startHour;
-          startHour = parseDate(startHour);
-          endHour = data[i].endHour;
-          endHour = parseDate(endHour);
-
-          // Création de l'event background
-          rendezvous = {
-            id:    'blocked',
-            start: startHour,
-            end: endHour,
-            overlap: false,
-            rendering: 'background',
-            color: 'red'
-          }; 
-          $scope.events.push(rendezvous);
-        }
-  }
-
-
-  // Reformate la date pour rendre compatible par FullCalendar
-  function parseDate (date){
-    var parsedDate = moment(date).format('YYYY[-]MM[-]DD[T]HH[:]mm[:]ss');
-    return parsedDate;
-  }
 
   /* Calcul dynamique de la date de début de semaine et de fin
    * weekProgression : un nombre indiquant si on avance ou recule dans le calendrier
@@ -280,144 +235,9 @@ angular.module('tbApp')
       currentWeekLastDay = moment().subtract(1, 'weeks').endOf('isoWeek')._d;
     }
 
-    //$log.debug(currentWeekFirstDay._d);
-    //$log.debug(currentWeekLastDay._d);
     $scope.currentWeek.firstDay = currentWeekFirstDay;
     $scope.currentWeek.lastDay = currentWeekLastDay;
     return $scope.currentWeek;
-  }
-
-  // Genère les evènements de background pour les heures d'ouvertures
-  // firstDayDate : une date qui correspond à la première date de la semaine
-  function createBusinessHoursEvents (firstDayDate){
-    $scope.businessHours.splice(0, $scope.businessHours.length);
-    var day = firstDayDate;
-    var businessHour = {};
-    var startHour = '';
-    var endHour = '';
-
-    for (var i = 0; i <= $scope.schedules.length - 1; i++) {
-
-      // On créer uniquement l'évènement les jours d'ouverture
-      if ($scope.schedules[i].workingDay === true) {
-
-        // Première boucle, on créé l'event directement
-        if (i === 0) {
-          // de minuit à l'ouverture
-          var hoursToStart = 0;
-          var hourToFinish = parseScheduleHours($scope.schedules[i].startHour);
-          startHour = day;
-          endHour = moment(day).add(hourToFinish, 'hours')._d;
-          startHour = parseDate(startHour);
-          endHour = parseDate(endHour);
-
-          businessHour = {
-            id:    'closed',
-            start: startHour,
-            end: endHour,
-            overlap: false,
-            rendering: 'background',
-            color: 'grey'
-          };
-          $scope.businessHours.push(businessHour);
-
-          // de la fermeture au jour suivant (ouverture)
-          var dayAfter = moment(day).add(1, 'days')._d;
-          hoursToStart = parseScheduleHours($scope.schedules[i].endHour);
-          hourToFinish = parseScheduleHours($scope.schedules[i+1].startHour);
-          startHour = moment(day).add(hoursToStart, 'hours')._d;
-          endHour = moment(dayAfter).add(hourToFinish, 'hours')._d;
-          startHour = parseDate(startHour);
-          endHour = parseDate(endHour);
-
-          businessHour = {
-            id:    'closed',
-            start: startHour,
-            end: endHour,
-            overlap: false,
-            rendering: 'background',
-            color: 'grey'
-          };
-          $scope.businessHours.push(businessHour);
-        }
-
-        // Boucles suivantes, on créé l'event après avoir ajouté un jour
-        else {
-          day = moment(day).add(1, 'days')._d;
-
-          // de la fermeture au jour suivant (ouverture)
-          dayAfter = moment(day).add(1, 'days')._d;
-          hoursToStart = parseScheduleHours($scope.schedules[i].endHour);
-          hourToFinish = parseScheduleHours($scope.schedules[i+1].startHour);
-          startHour = moment(day).add(hoursToStart, 'hours')._d;
-          endHour = moment(dayAfter).add(hourToFinish, 'hours')._d;
-          startHour = parseDate(startHour);
-          endHour = parseDate(endHour);
-
-          businessHour = {
-            id:    'closed',
-            start: startHour,
-            end: endHour,
-            overlap: false,
-            rendering: 'background',
-            color: 'grey'
-          };
-          $scope.businessHours.push(businessHour);
-        }
-
-        if (i === ($scope.schedules.length-1)) {
-          // de la fermeture à miniut
-          hoursToStart = parseScheduleHours($scope.schedules[i].endHour);
-          hourToFinish = 0;
-          startHour = moment(day).add(hourToFinish, 'hours')._d;
-          endHour = moment(day).endOf('day')._d;
-          startHour = parseDate(startHour);
-          endHour = parseDate(endHour);
-
-          businessHour = {
-            id:    'closed',
-            start: startHour,
-            end: endHour,
-            overlap: false,
-            rendering: 'background',
-            color: 'grey'
-          };
-          $scope.businessHours.push(businessHour);
-        }
-
-      } 
-      // les jours fermé, créer des events full-day
-      else {
-        day = moment(day).add(1, 'days')._d;
-
-        startHour = day;
-        endHour = moment(day).endOf('day')._d;
-        startHour = parseDate(startHour);
-        endHour = parseDate(endHour);
-
-        businessHour = {
-          id:    'closed',
-          start: startHour,
-          end: endHour,
-          overlap: false,
-          rendering: 'background',
-          color: 'grey'
-        };
-        $scope.businessHours.push(businessHour);
-      }
-    }
-
-  }
-
-  function parseScheduleHours (string){
-    var hour = String(string);
-    hour = hour.substring(0, 2);
-    if (hour.charAt(0) === '0') {
-      hour = hour.substring(1);
-    }
-    hour = Number(hour);
-    //$log.debug(hour);
-    return hour;
   }
 
   // Current date variables
@@ -427,46 +247,6 @@ angular.module('tbApp')
   var y = date.getFullYear();
   //var h = date.getHours() + ':00:00';
   var h = '7:00:00';
-
-  // Generate an event with the prestation duration and the default hour
-  function createMyRendezvousEvent(date){
-
-    if ($scope.myRendezvous.length > 0) {
-      $scope.myRendezvous.splice(0, 1);
-      //$log.debug('first element removed');
-    }
-
-    var date = date;
-    var d = date.getDate();
-    var m = date.getMonth();
-    var y = date.getFullYear();
-
-    var prestationDuration = getItem('rendezvous').prestation.duration;
-    var defaultHour = 8;
-    var finalHour = 8;
-    var finalMinutes = '';
-  
-    for (var i = prestationDuration; i >= 0; i -= 60) {
-      if (i >= 60) {
-        finalHour++;
-      }
-      if (i < 60) {
-        finalMinutes = i;
-      }
-    }
-
-  // my rendez-vous
-    var rendezvousToBook = {
-        id: 'myRdv',
-        title : 'Votre rendez-vous',
-        start : new Date(y, m, d, defaultHour, 0),
-        end    : new Date(y, m, d, finalHour, finalMinutes),
-        color: 'green'
-      };
-
-    $scope.myRendezvous.push(rendezvousToBook);
-  }
-
 
   /* config object */
   $scope.uiConfig = {
@@ -510,18 +290,16 @@ angular.module('tbApp')
           $scope.formData.businessId,
           request,
           function (res){
-            var array = res.rendezvous;
             //$log.debug(res);
+            var array = res.rendezvous;
 
-            createRendezvousTakenEvents(array);
-            //$log.debug($scope.events);
-            
-            createBusinessHoursEvents(startDay);
-            createMyRendezvousEvent(startDay);
+            $scope.events = CalendarService.createRendezvousTakenEvents($scope.events, array);            
+            $scope.businessHours = CalendarService.createBusinessHoursEvents($scope.businessHours, $scope.schedules, startDay);
+            $scope.myRendezvous = CalendarService.createMyRendezvousEvent($scope.myRendezvous, startDay);
 
-            //$log.debug($scope.myRendezvous);
-            //$log.debug($scope.events);
-            //$log.debug($scope.businessHours);
+            $log.debug($scope.events);
+            $log.debug($scope.businessHours);
+            $log.debug($scope.myRendezvous);
           },
 
           function(error){
---
+++ client/components/services/calendar.service.js
@@ -0,0 +1,236 @@
+'use strict';
+
+angular.module('tbApp')
+  .factory('CalendarService', function(uiCalendarConfig, localStorageService){
+
+    /******************
+    * Private functions
+    *******************/
+
+    function parseDate(date){
+      var parsedDate = moment(date).format('YYYY[-]MM[-]DD[T]HH[:]mm[:]ss');
+      return parsedDate;
+    }
+
+    function parseScheduleHours(string){
+      var hour = String(string);
+      hour = hour.substring(0, 2);
+      if (hour.charAt(0) === '0') {
+        hour = hour.substring(1);
+      }
+      hour = Number(hour);
+      //$log.debug(hour);
+      return hour;
+    }
+
+    // Accès au local storage
+    function getItem(key) {
+      return localStorageService.get(key);
+    }
+
+    /*****************
+    * Public functions
+    ******************/
+    return {
+      initCalendar: function(){
+
+      },
+
+      // Créé des évènements aux rendez-vous déjà prit
+      createRendezvousTakenEvents: function(scope, array){
+        var scopeEvents = scope;
+        scopeEvents.splice(0, scopeEvents.length);
+        var data = array;
+
+        var rendezvous = [];
+        var startHour = '';
+        var endHour = '';
+        for (var i = data.length - 1; i >= 0; i--) {
+          // Converstion pour des dates compatibles avec le calendrier
+          startHour = data[i].startHour;
+          startHour = parseDate(startHour);
+          endHour = data[i].endHour;
+          endHour = parseDate(endHour);
+
+          // Création de l'event background
+          rendezvous = {
+            id:    'blocked',
+            start: startHour,
+            end: endHour,
+            overlap: false,
+            rendering: 'background',
+            color: 'red'
+          }; 
+          scopeEvents.push(rendezvous);
+        }
+        return scopeEvents;
+      },
+
+      // Genère les evènements de background pour les heures d'ouvertures
+      // firstDayDate : une date qui correspond à la première date de la semaine
+      // scopeEvents : le scope qui va afficher les events dans le calendrier
+      // schedules : les horaires d'un salon
+      createBusinessHoursEvents: function(scopeEvents, schedules, firstDayDate){
+        var scopeBusinessHours = scopeEvents;
+        var scopeSchedules = schedules;
+
+        scopeBusinessHours.splice(0, scopeBusinessHours.length);
+        var day = firstDayDate;
+        var businessHour = {};
+        var startHour = '';
+        var endHour = '';
+
+        for (var i = 0; i <= scopeSchedules.length - 1; i++) {
+
+          // On créer uniquement l'évènement les jours d'ouverture
+          if (scopeSchedules[i].workingDay === true) {
+
+            // Première boucle, on créé l'event directement
+            if (i === 0) {
+              // de minuit à l'ouverture
+              var hoursToStart = 0;
+              var hourToFinish = parseScheduleHours(scopeSchedules[i].startHour);
+              startHour = day;
+              endHour = moment(day).add(hourToFinish, 'hours')._d;
+              startHour = parseDate(startHour);
+              endHour = parseDate(endHour);
+
+              businessHour = {
+                id:    'closed',
+                start: startHour,
+                end: endHour,
+                overlap: false,
+                rendering: 'background',
+                color: 'grey'
+              };
+              scopeBusinessHours.push(businessHour);
+
+              // de la fermeture au jour suivant (ouverture)
+              var dayAfter = moment(day).add(1, 'days')._d;
+              hoursToStart = parseScheduleHours(scopeSchedules[i].endHour);
+              hourToFinish = parseScheduleHours(scopeSchedules[i+1].startHour);
+              startHour = moment(day).add(hoursToStart, 'hours')._d;
+              endHour = moment(dayAfter).add(hourToFinish, 'hours')._d;
+              startHour = parseDate(startHour);
+              endHour = parseDate(endHour);
+
+              businessHour = {
+                id:    'closed',
+                start: startHour,
+                end: endHour,
+                overlap: false,
+                rendering: 'background',
+                color: 'grey'
+              };
+              scopeBusinessHours.push(businessHour);
+            }
+
+            // Boucles suivantes, on créé l'event après avoir ajouté un jour
+            else {
+              day = moment(day).add(1, 'days')._d;
+
+              // de la fermeture au jour suivant (ouverture)
+              dayAfter = moment(day).add(1, 'days')._d;
+              hoursToStart = parseScheduleHours(scopeSchedules[i].endHour);
+              hourToFinish = parseScheduleHours(scopeSchedules[i+1].startHour);
+              startHour = moment(day).add(hoursToStart, 'hours')._d;
+              endHour = moment(dayAfter).add(hourToFinish, 'hours')._d;
+              startHour = parseDate(startHour);
+              endHour = parseDate(endHour);
+
+              businessHour = {
+                id:    'closed',
+                start: startHour,
+                end: endHour,
+                overlap: false,
+                rendering: 'background',
+                color: 'grey'
+              };
+              scopeBusinessHours.push(businessHour);
+            }
+
+            if (i === (scopeSchedules.length-1)) {
+              // de la fermeture à miniut
+              hoursToStart = parseScheduleHours(scopeSchedules[i].endHour);
+              hourToFinish = 0;
+              startHour = moment(day).add(hourToFinish, 'hours')._d;
+              endHour = moment(day).endOf('day')._d;
+              startHour = parseDate(startHour);
+              endHour = parseDate(endHour);
+
+              businessHour = {
+                id:    'closed',
+                start: startHour,
+                end: endHour,
+                overlap: false,
+                rendering: 'background',
+                color: 'grey'
+              };
+              scopeBusinessHours.push(businessHour);
+            }
+
+          } 
+          // les jours fermé, créer des events full-day
+          else {
+            day = moment(day).add(1, 'days')._d;
+
+            startHour = day;
+            endHour = moment(day).endOf('day')._d;
+            startHour = parseDate(startHour);
+            endHour = parseDate(endHour);
+
+            businessHour = {
+              id:    'closed',
+              start: startHour,
+              end: endHour,
+              overlap: false,
+              rendering: 'background',
+              color: 'grey'
+            };
+            scopeBusinessHours.push(businessHour);
+          }
+        }
+        return scopeBusinessHours;
+      },
+
+      // Generate an event with the prestation duration and the default hour
+      createMyRendezvousEvent: function(scope, date){
+        var scopeMyRendezvous = scope;
+        if (scopeMyRendezvous.length > 0) {
+          scopeMyRendezvous.splice(0, 1);
+          //$log.debug('first element removed');
+        }
+
+        var date = date;
+        var d = date.getDate();
+        var m = date.getMonth();
+        var y = date.getFullYear();
+
+        var prestationDuration = getItem('rendezvous').prestation.duration;
+        var defaultHour = 8;
+        var finalHour = 8;
+        var finalMinutes = '';
+      
+        for (var i = prestationDuration; i >= 0; i -= 60) {
+          if (i >= 60) {
+            finalHour++;
+          }
+          if (i < 60) {
+            finalMinutes = i;
+          }
+        }
+
+      // my rendez-vous
+        var rendezvousToBook = {
+            id: 'myRdv',
+            title : 'Votre rendez-vous',
+            start : new Date(y, m, d, defaultHour, 0),
+            end    : new Date(y, m, d, finalHour, finalMinutes),
+            color: 'green'
+          };
+
+        scopeMyRendezvous.push(rendezvousToBook);
+        return scopeMyRendezvous;
+      }
+    };
+  });
