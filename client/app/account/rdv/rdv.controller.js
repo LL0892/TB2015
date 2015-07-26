e57b3c0d88dae6cedd56353f0c606f8df320574c
Laurent Lienher <laurent.lienher@gmail.com>
2015-07-26 14:25:13 +0200

    clean code

--- client/app/account/rdv/rdv.controller.js
+++ client/app/account/rdv/rdv.controller.js
@@ -4,6 +4,7 @@ angular.module('tbApp')
   .controller('MyRendezvousCtrl', function ($scope, $log, $http, $timeout, Urls) {
 
     $http.get(Urls.api + 'rendezvous').success(function(data){
+      $log.debug(data);
       $scope.rendezvous = data;
 
       for (var i = $scope.rendezvous.length - 1; i >= 0; i--) {
